d720828208a20d47769cda6478abd23be900ae0b
Gustav Vang Klinker <guk001@edu.zealand.dk>
2025-04-02 15:19:40 +0200

    Auto generated code

--- IMDBFrontend/Program.cs
+++ IMDBFrontend/Program.cs
@@ -1,4 +1,37 @@
-﻿int Menu()
+﻿using Microsoft.Data.SqlClient;
+
+string connectionString = "Server=localhost;Database=IMDB;";
+int choice = 0;
+do
+{
+    choice = Menu();
+    switch (choice)
+    {
+        case 1:
+            SearchMovie();
+            break;
+        case 2:
+            SearchPerson();
+            break;
+        case 3:
+            AddMovie();
+            break;
+        case 4:
+            DeleteMovie();
+            break;
+        case 5:
+            UpdateMovie();
+            break;
+        case 6:
+            Console.WriteLine("Goodbye!");
+            return;
+        default:
+            Console.WriteLine("Invalid choice");
+            break;
+    }
+} while (choice != 6);
+
+int Menu()
 {
     Console.WriteLine("Welcome to IMDB \n" +
         "Pick an option");
@@ -14,3 +47,87 @@
     return choice;
 }
 
+void SearchMovie()
+{
+    Console.WriteLine("Enter the name of the movie");
+    string movieName = Console.ReadLine();
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        connection.Open();
+        string query = "SELECT * FROM Movies WHERE Name like %@Name%";
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@Name", movieName);
+        SqlDataReader reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            Console.WriteLine(reader["Name"]);
+        }
+    }
+}
+
+void SearchPerson()
+{
+    Console.WriteLine("Enter the name of the person");
+    string personName = Console.ReadLine();
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        connection.Open();
+        string query = "SELECT * FROM Persons WHERE Name like %@Name%";
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@Name", personName);
+        SqlDataReader reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            Console.WriteLine(reader["Name"]);
+        }
+    }
+}
+
+void AddMovie()
+{
+    Console.WriteLine("Enter the name of the movie");
+    string movieName = Console.ReadLine();
+    Console.WriteLine("Enter the year of the movie");
+    int year = Convert.ToInt32(Console.ReadLine());
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        connection.Open();
+        string query = "INSERT INTO Movies (Name, Year) VALUES (@Name, @Year)";
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@Name", movieName);
+        command.Parameters.AddWithValue("@Year", year);
+        command.ExecuteNonQuery();
+    }
+}
+
+
+void DeleteMovie()
+{
+    Console.WriteLine("Enter the name of the movie to delete");
+    string movieName = Console.ReadLine();
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        connection.Open();
+        string query = "DELETE FROM Movies WHERE Name = @Name";
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@Name", movieName);
+        command.ExecuteNonQuery();
+    }
+}
+
+void UpdateMovie()
+{
+    Console.WriteLine("Enter the name of the movie to update");
+    string movieName = Console.ReadLine();
+    Console.WriteLine("Enter the new year of the movie");
+    int year = Convert.ToInt32(Console.ReadLine());
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        connection.Open();
+        string query = "UPDATE Titles SET Year = @Year WHERE Name = @Name";
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@Name", movieName);
+        command.Parameters.AddWithValue("@Year", year);
+        command.ExecuteNonQuery();
+    }
+}
