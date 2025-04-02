f7b5733593f81429992f93104c22055711a213be
Gustav Vang Klinker <guk001@edu.zealand.dk>
2025-04-02 14:40:59 +0200

    Add project files.

---
+++ IMDBFrontend/Program.cs
@@ -0,0 +1,13 @@
+﻿void Menu()
+{
+    Console.WriteLine("Welcome to IMDB \n" +
+        "Pick an option");
+    Console.WriteLine("1. Search for a movie");
+    Console.WriteLine("2. Search for a person");
+    Console.WriteLine("3. Add a movie");
+    Console.WriteLine("4. Add a person");
+    Console.WriteLine("5. Delete a movie");
+    Console.WriteLine("6. Update a movie");
+    Console.WriteLine("7. Exit");
+}
+
