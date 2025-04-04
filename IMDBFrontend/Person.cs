d2af8ba78a7aaef341f134f8cc843e2ae1b76e23
Gustav Vang Klinker <guk001@edu.zealand.dk>
2025-04-04 12:48:48 +0200

    Model classes made

---
+++ IMDBFrontend/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDBFrontend
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Nconst { get; set; }
+        public List<string> Professions { get; set; }
+        public int BirthYear { get; set; }
+        public int DeathYear { get; set; }
+        public Person() {
+            Professions = new List<string>();
+        }
+    }
+}
---
+++ IMDBFrontend/Title.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDBFrontend
+{
+    public class Title
+    {
+        public int Id { get; set; }
+        public string PrimaryTitle { get; set; }
+        public string OriginalTitle { get; set; }
+        public string Tconst { get; set; }
+        public string Type { get; set; }
+        public List<string> Genres { get; set; }
+        public int StartYear { get; set; }
+        public int EndYear { get; set; }
+        public Title() {
+            Genres = new List<string>();
+        }
+    }
+}
