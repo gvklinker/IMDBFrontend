21a4fd0c00e6b58d95cc01cc790771806d2674a2
Gustav Vang Klinker <guk001@edu.zealand.dk>
2025-04-06 12:01:56 +0200

    Update storets procedure in use

--- IMDBFrontend/Program.cs
+++ IMDBFrontend/Program.cs
@@ -7,6 +7,7 @@ int choice = 0;
 do
 {
     choice = Menu();
+    Console.Clear();
     switch (choice)
     {
         case 1:
@@ -37,11 +38,11 @@ int Menu()
 {
     Console.WriteLine("Welcome to IMDB \n" +
         "Pick an option");
-    Console.WriteLine("1. Search for a movie");
+    Console.WriteLine("1. Search for a title");
     Console.WriteLine("2. Search for a person");
-    Console.WriteLine("3. Add a movie");
-    Console.WriteLine("4. Delete a movie");
-    Console.WriteLine("5. Update a movie");
+    Console.WriteLine("3. Add a title");
+    Console.WriteLine("4. Delete a title");
+    Console.WriteLine("5. Update a title");
     Console.WriteLine("6. Exit");
 
     int choice = Convert.ToInt32(Console.ReadLine());
@@ -50,7 +51,7 @@ int Menu()
 
 void SearchMovie()
 {
-    Console.WriteLine("Enter the name of the movie");
+    Console.WriteLine("Enter the name of the title");
     string movieName = Console.ReadLine();
     using (SqlConnection connection = new SqlConnection(connectionString))
     {
@@ -88,7 +89,7 @@ void SearchPerson()
 
 void AddMovie()
 {
-    Console.WriteLine("Enter the name of the movie");
+    Console.WriteLine("Enter the name of the title");
     string movieName = Console.ReadLine();
     Console.WriteLine("Enter the year of the movie");
     int year = Convert.ToInt32(Console.ReadLine());
@@ -107,7 +108,7 @@ void AddMovie()
 
 void DeleteMovie()
 {
-    Console.WriteLine("Enter the Tconst of the movie to delete");
+    Console.WriteLine("Enter the Tconst of the title you want to delete");
     string tconst = Console.ReadLine();
     using (SqlConnection connection = new SqlConnection(connectionString))
     {
@@ -122,17 +123,35 @@ void DeleteMovie()
 
 void UpdateMovie()
 {
-    Console.WriteLine("Enter the name of the movie to update");
-    string movieName = Console.ReadLine();
-    Console.WriteLine("Enter the new year of the movie");
+    Console.WriteLine("Enter the tconst of the title you want to update");
+    string tconst = Console.ReadLine();
+    Console.WriteLine("Enter the new primary title of the title");
+    string name = Console.ReadLine();
+    Console.WriteLine("Enter the new original title of the title");
+    string originalTitle = Console.ReadLine();
+    Console.WriteLine("Enter the new start year of the title");
     int year = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter the new end year of the title");
+    int endYear = Convert.ToInt32(Console.ReadLine());
+    //Console.WriteLine("Enter the new type of the title");
+    Console.WriteLine("Enter the new value for whether the title is for adults only (1 if it is, 0 if it isn't)");
+    int isAdult = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter the new runtime in minutes for the title");
+    int runtime = Convert.ToInt32(Console.ReadLine());
+
     using (SqlConnection connection = new SqlConnection(connectionString))
     {
         connection.Open();
-        string query = "UPDATE Titles SET Year = @Year WHERE PrimaryTitle = @Name";
-        SqlCommand command = new SqlCommand(query, connection);
-        command.Parameters.AddWithValue("@Name", movieName);
-        command.Parameters.AddWithValue("@Year", year);
+        //string query = "UPDATE Titles SET Year = @Year WHERE PrimaryTitle = @tconst";
+        SqlCommand command = new SqlCommand("UpdateTitle", connection);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.Add(new SqlParameter("@Tconst", tconst));
+        command.Parameters.Add(new SqlParameter("@Name", name));
+        command.Parameters.Add(new SqlParameter("@Year", year));
+        command.Parameters.Add(new SqlParameter("@EndYear", endYear));
+        command.Parameters.Add(new SqlParameter("@OriginalTitle", originalTitle));
+        command.Parameters.Add(new SqlParameter("@IsAdult", isAdult));
+        command.Parameters.Add(new SqlParameter("@Runtime", runtime));
         command.ExecuteNonQuery();
         connection.Close();
     }
